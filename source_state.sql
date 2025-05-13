3c0441dd47ab83180c686927c5c620a3359ebfa9
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-05-13 19:28:26 +0530

    global_fcl_shipment_initiation
    13-05-2025

---
+++ source_state.sql
@@ -0,0 +1,3 @@
+select source,state from public.shipments where id = '399a7100-7111-47a3-948c-d6faa4b27542';
+
+SELECT id, '"contract"' AS source FROM shipments WHERE id = '399a7100-7111-47a3-948c-d6faa4b27542';
